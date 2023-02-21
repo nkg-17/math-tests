21f30744757ea94c5f865d4dd142ec53f70f535d
Ts. Vadim <tsentsevitskiy.v@gmail.com>
2023-02-21 11:20:24 +0700

    Added 'answer: ' text to Test.Solution

--- src/pages/Test/Solution.jsx
+++ src/pages/Test/Solution.jsx
@@ -1,15 +1,22 @@
 import { ArticleLayout } from 'components/layouts';
 
 
-function Solution({className, style, test}) {
+function Solution({test}) {
 	return (
 		<>
 			<ArticleLayout.Heading color="border-emerald-400">
                 Решение
             </ArticleLayout.Heading>
-			<p className={`w-full text-lg text-start text-slate-900 first-letter:ml-10 ${className}`}
-			style={style}>
+			<p className={`w-full text-lg text-start text-slate-900 first-letter:ml-10`}>
 				{test.solution.text}
+                <br />
+                {
+                    (test.answer) ? (
+                        <span className="w-full ml-10">
+                            <span className="font-bold">Ответ: </span>{test.answer}
+                        </span>
+                    ) : (<></>)
+                }
 			</p>
 		</>
 	)
