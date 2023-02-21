f09bb96e7633af777898f2cd81a5e0d917d4aa00
Ts. Vadim <tsentsevitskiy.v@gmail.com>
2023-02-21 11:14:44 +0700

    Simplified Test.SubmitForm

--- src/pages/Test/SubmitForm.jsx
+++ src/pages/Test/SubmitForm.jsx
@@ -12,38 +12,47 @@ import { RoutePaths } from 'utils/constants'
 function SubmitForm({test, className, style}) {
     const context = useContext(TestContext)
 
+    const inputFieldAndSubmitButton = (
+        <>
+            <InputField placeholder="Ответ" />
+            {
+                (context.solutionOpened) ? (
+                    <LinkButton to={RoutePaths.Catalog} variant="primary">
+                        Следующая
+                    </LinkButton>
+                ) : (
+                    <Button variant="success">Проверить</Button>
+                )
+            }
+        </>
+    )
+
+    const openSolutionButton = (
+        <>
+            {
+                (!context.solutionOpened) ? (
+                    <Button 
+                    onClick={context.openSolution}
+                    to={RoutePaths.Catalog} 
+                    variant="success">
+                        Открыть Решение
+                    </Button>
+                ) : (<></>)
+            }
+        </>
+    )
+
 	return (
-		<div className={`
-		w-full py-6 
-		flex flex-row align-center justify-between
-		 
-		${className}`} 
+		<div className={`w-full py-6 columns-3 ${className}`} 
 		style={style}>
-			<div className="flex flex-row gap-2">
-				<LinkButton to={RoutePaths.Catalog} variant="secondary">Вернуться</LinkButton>
-                {
-                    (!context.solutionOpened) ? (
-                        <Button 
-                        onClick={context.openSolution}
-                        to={RoutePaths.Catalog} 
-                        variant="dark">
-                            Открыть Решение
-                        </Button>
-                    ) : (<></>)
-                }
-				
+			<div className="">
+				<LinkButton to={RoutePaths.Catalog} variant="secondary">
+                    Вернуться
+                </LinkButton>
 			</div>
 
-			<div className="flex flex-row gap-2">
-				<InputField placeholder="Ответ" />
-                {
-                    (context.solutionOpened) ? (
-                        <LinkButton to={RoutePaths.Catalog} variant="primary">Следующая</LinkButton>
-                    ) : (
-                        <Button variant="success">Проверить</Button>
-                    )
-                }
-				
+			<div className="flex flex-row justify-center">
+				{openSolutionButton}
 			</div>
 		</div>
 	)
